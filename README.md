07e197a755c6a4e58968b8c098bdc6178520aed1
Annahlu <analuizamartinscesario@gmail.com>
2021-10-31 13:56:19 -0300

    Ultimas alterações

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Projeto durante o SheHacks2021
+# Projeto durante o SheHacks2021 - Manducare
+
+### Manducare significa comer em latim e sabemos que o acesso a alimentos de qualidade é um direito de todos. A plataforma surge como um meio de incentivar o comércio local, justo e solidário, prezando pelos agricultores familiares que são responsáveis por todas as etapas do processo de produção e pelos compradores.
+
+### A beterraba é um alimento rico em ferro e acido fólico sendo assim, atua diretamente no combate a anemia, o que vai de encontro a nossa missão, promover segurança alimentar. Por isso criamos a Bete, nossa mascote virtual para auxiliar compradores e vendedores a entenderem melhor a nossa proposta.
+
+### 
 
 ## Por: Ana Luiza Martins, Aya Esther, Catherine Louise e Thiane Deprá.
--- assets/js/pesquisar.js
+++ assets/js/pesquisar.js
@@ -5,7 +5,13 @@ function Search(){
 
         document.getElementById("status").innerHTML = "Estamos disponíveis na sua cidade! Aproveite nossas opções!";
         document.getElementById("mapa").innerHTML = "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d140784.94066812636!2d-47.98461525799531!3d-21.99027924808511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b8712776c00717%3A0xd818e033f86079d2!2sEstr.%20Mun.%20Guilherme%20Scatena%2C%20S%C3%A3o%20Carlos%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1635645129319!5m2!1spt-BR!2sbr' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy'></iframe>";
-        //document.getElementById("btnLoja").innerHTML = "<button type='button' class='btn btn-primary' href='venda.html'> Ir para a loja </button>";
+        document.getElementById("load").innerHTML = "<img class='img-fluid' src='assets/img/loading.gif'>";
+        
+        setTimeout(function(){ 
+            document.getElementById("load").innerHTML =" ";
+            console.log("entrou");
+        }, 2100);
+
         document.getElementById("btnLoja").innerHTML =  "<a class='btn btn-primary' href='venda.html' role='button'>  Ir para a loja </a>";
 
     } else {
